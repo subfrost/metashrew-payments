2569fb27dd15e4a6e30908a2c16649b4e11085c4
ignoblehierarch <ignoblehierarch@proton.me>
2024-07-30 15:03:40 -0500

    wip: added payment indexing and associated tables

---
+++ assembly/indexer/Payment.ts
@@ -0,0 +1,101 @@
+import { Block } from "metashrew-as/assembly/blockdata/block";
+import { Input, Output } from "metashrew-as/assembly/blockdata/transaction";
+import { OUTPOINT_TO_OUTPUT } from "metashrew-spendables/assembly/tables";
+import { PAYMENTS_TABLE } from "../tables/tables";
+import { Box } from "metashrew-as/assembly/utils/box";
+
+function intoAddress(output: Output): ArrayBuffer {
+  const address = output.intoAddress();
+  if (address === null) return String.UTF8.encode("UNSPENDABLE");
+  return address as ArrayBuffer;
+}
+
+export function bytesToOutput(v: ArrayBuffer): Output {
+  const output = new Output(Box.from(v));
+  return output;
+}
+
+@unmanaged
+export class PaymentTuple {
+  senders: ArrayBuffer[];
+  amount: u64;
+  constructor(senders: ArrayBuffer[], amount: u64) {
+    this.senders = senders;
+    this.amount = amount;
+  };
+};
+export class PaymentsIndex {
+  static indexBlock(height: u32, block: Block): void {
+    for (let i = 0; i < block.transactions.length; i++) {
+      const tx = block.transactions[i];
+      let inputs = tx.ins;
+      let inputIndex = 0;
+      // amts are 1:1 with inputs
+      let inputAmounts = this.getInputAmounts(inputs);
+      for (let j = 0; j < tx.outs.length; j++) {
+        const output = tx.outs[j];
+        let amountRemaining = output.value;
+        while (amountRemaining > 0 && inputIndex < inputs.length) {
+          const curr = inputs[inputIndex];
+          const amt = inputAmounts[inputIndex];
+          const diff = amt - amountRemaining;
+          // if the input amt covers the entire amt of the output, then
+          if (diff > 0) {
+            amountRemaining = 0;
+            inputAmounts[inputIndex] = diff;
+          } else if (diff < 0) {
+            // we have used up all the sats in the current input, we can move to the next one
+            amountRemaining -= amt;
+            inputAmounts[inputIndex] = 0;
+            inputIndex++;
+          }
+          // it should never equal zero because there must be excess to pay for fees
+          // now we can save the payment
+          const inputAddr = intoAddress(
+            bytesToOutput(
+              OUTPOINT_TO_OUTPUT.select(
+                curr.previousOutput().toArrayBuffer(),
+              ).unwrap(),
+            ),
+          );
+          const recipientPointer = PAYMENTS_TABLE.selectValue<u32>(height)
+            .keyword("/")
+            .select(intoAddress(output));
+          const ptr = recipientPointer
+            .keyword("/")
+            .select(inputAddr as ArrayBuffer);
+          if (ptr.length() == 0) {
+            recipientPointer.append(inputAddr as ArrayBuffer);
+          }
+          ptr.appendValue<u64>(output.value);
+        }
+      }
+    }
+  }
+ 
+
+  // gets the senders and the amounts sent to a specific address
+  static paymentsToAddress(height: u32, address: ArrayBuffer): PaymentTuple {
+    const recipientPtr = PAYMENTS_TABLE.selectValue<u32>(height).keyword("/").select(address);
+    const senderList = recipientPtr.getList();
+    let senders = new Array<ArrayBuffer>(senderList.length);
+    let totalReceived: u64  = 0;
+    for(let i = 0; i < senderList.length; i++) {
+      senders[i] = senderList[i];
+      const amt = recipientPtr.keyword("/").select(senders[i]).getValue<u64>();
+      totalReceived += amt;
+    }
+    return new PaymentTuple(senders, totalReceived);
+  }
+
+  // provide the amount of sats in each input by using previous output
+  static getInputAmounts(inputs: Input[]): Array<u64> {
+    let amts = new Array<u64>(inputs.length);
+    for (let i = 0; i < inputs.length; i++) {
+      const prev_out = inputs[i].previousOutput().toArrayBuffer();
+      const output = OUTPOINT_TO_OUTPUT.select(prev_out).unwrap();
+      amts[i] = bytesToOutput(output).value;
+    }
+    return amts;
+  }
+}
---
+++ assembly/tables/tables.ts
@@ -0,0 +1,4 @@
+import { IndexPointer } from "metashrew-as/assembly/indexer/tables";
+
+export const SENDER_TO_RECIPIENT = IndexPointer.for("/senders/to/recipients");
+export const PAYMENTS_TABLE = IndexPointer.for("/payments/");
