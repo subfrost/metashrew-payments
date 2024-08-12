be1cc6a66993408632e4a978ff2c074f9bb065fc
ignoblehierarch <ignoblehierarch@proton.me>
2024-08-12 15:28:03 -0500

    update

--- tests/release.spec.ts
+++ tests/release.spec.ts
@@ -1,11 +1,11 @@
-/*
+
 import { expect } from "chai";
 import fs from "fs-extra";
 //@ts-ignore
 import bitcoinjs = require("bitcoinjs-lib");
 import { EventEmitter } from "events";
 import { IndexerProgram } from "metashrew-test";
-/*
+
 import { buildProgram } from "metashrew-runes/lib/tests/utils/general"
 import path from "path";
 import {
@@ -13,14 +13,64 @@ import {
   buildTransaction,
 } from "metashrew-runes/lib/tests/utils/block-helpers";
 import { DEBUG_WASM } from "metashrew-runes/lib/tests/utils/general";
-*/
+import { sendersPerPayment } from "../assembly/view/payment";
+import { input } from "metashrew-as/assembly/indexer";
 
 describe("metashrew-payments", () => {
-  it("indexes a single payment with multiplke inputs and a single output", async () => {
-	  /*
+  it("indexes a single payment with multiple inputs and a single output", async () => {
+	
     const program = buildProgram(DEBUG_WASM);
     const height = 853768;
     program.setBlockHeight(height);
+    const prevIns2 = [
+      {
+        inputTxHash: Buffer.from('2f093cf7e647991d81370297249e9abede6bf3ab29384b1f1b622e1d17599e18', "hex"),
+        inputTxOutputIndex: 0,
+      },
+      {
+        inputTxHash: Buffer.from('2f093cf7e647991d81370297249e9abede6bf3ab29384b1f1b622e1d17599e18', "hex"),
+        inputTxOutputIndex: 1,
+      }
+    ];
+    const prevOuts2 = [
+      {
+        script: bitcoinjs.payments.p2pkh({
+          address: 'bc1q3y8zn4m74cg5ctvvc7f9t87wjnrqdm5fguyvw7',
+          network: bitcoinjs.networks.bitcoin,
+        }).output,
+        value: 50000000,
+      },
+      {
+        script: bitcoinjs.payments.p2pkh({
+          address: '17XwVXXE6Y5U7QU4NdALFmKvjsEz7wJAcZ',
+          network: bitcoinjs.networks.bitcoin,
+        }).output,
+        value: 36329529,
+      }
+    ];
+    const prevIns1 = [
+      {
+        inputTxHash: Buffer.from('839f1ffda22c3ace710f88a8fbe84d4198fd8e5b804b341a30bc51f9559c9c2a', "hex"),
+        inputTxOutputIndex: 0,
+      }
+    ];
+    const prevOuts1 = [
+      {
+        script: bitcoinjs.payments.p2wpkh({
+          address: 'bc1qne8q7nk09ap48s5479a4wpht2hhldwzjvsclde',
+          network: bitcoinjs.networks.bitcoin,
+        }).output,
+        value: 940269,
+      },
+      {
+        script: bitcoinjs.payments.p2pkh({
+          address: '14SyhvDNCVqdGo83XqU5fcjKgWve8KaFX9',
+          network: bitcoinjs.networks.bitcoin,
+        }).output,
+        value: 9264,
+      }
+    ];
+
     const outputs = [
       {
         script: bitcoinjs.payments.p2wpkh({
@@ -35,17 +85,16 @@ describe("metashrew-payments", () => {
       {
         inputTxHash: Buffer.from('92574e7d0bbeb6dc02dbb4e783f799c6968ffff5417433fbab8b269a0af883c2', "hex"),
         inputTxOutputIndex: 0,
-        inputAmount: 9264,
       },
       {
         inputTxHash: Buffer.from('92574e7d0bbeb6dc02dbb4e783f799c6968ffff5417433fbab8b269a0af883c2', "hex"),
         inputTxOutputIndex: 1,
-        inputAmount: 4615706,
       },
     ];
 
     const transaction = buildTransaction(inputs, outputs);
     const block = buildDefaultBlock();
+    const prevBlock = buildDefaultBlock();
     block.transactions?.push(transaction);
     program.setBlock(block.toHex());
     await program.run("_start");
@@ -53,6 +102,6 @@ describe("metashrew-payments", () => {
     console.log(result);
     expect(true);
 
- */
+
   });
 });
---
+++ tests/utils/payment-helpers.ts
@@ -0,0 +1,9 @@
+import { IndexerProgram } from "metashrew-test";
+
+export const sendersperpayment = async (
+    program: IndexerProgram,
+    height: string,
+
+): Promise<any> => {
+
+}
